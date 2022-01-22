4e63d312c40c6747e948383fd10ec2c034ca8887
Chemmy <54594490+chemmy-jack@users.noreply.github.com>
2022-01-23 00:27:54 +0800

    correct typo of sh comments

--- install_requirement.sh
+++ install_requirement.sh
@@ -1,14 +1,14 @@
-// Vundle
+# Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-// tpm for tmux
+# tpm for tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-// oh-my-zsh
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-// powerlevel10k
+# powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-// zsh-autosuggestions
+# zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
