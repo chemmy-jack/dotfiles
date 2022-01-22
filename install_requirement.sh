42055a09b7da0d4b61b77c2d0eb962d800227189
Chemmy <54594490+chemmy-jack@users.noreply.github.com>
2022-01-23 00:26:25 +0800

    correct zsh-autosuggestions typo

--- install_requirement.sh
+++ install_requirement.sh
@@ -10,5 +10,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 // powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-// zsh-autosuggest
+// zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
