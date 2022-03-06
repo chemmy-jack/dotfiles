5f7fd0c460a397ec3e49c87aefc34c85224b0a1a
郭哲明 <jack@Jack-MacBook-Air.local>
2022-03-06 15:53:15 +0800

    add zsh syntax highligt

--- install_requirement.sh
+++ install_requirement.sh
@@ -16,6 +16,9 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
